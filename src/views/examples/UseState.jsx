10f5436e0d1f5c9401d475466b40a659c70bebc9
andytxr <andreluiiz.gst@gmail.com>
2022-05-16 17:48:36 -0300

    First commit

---
+++ src/views/examples/UseState.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { useState } from 'react';
+
+import PageTitle from '../../components/layout/PageTitle';
+import SectionTitle from '../../components/layout/SectionTitle';
+
+const UseState = (props) => {
+
+    let [x, setX] = useState(0)
+
+
+    return (
+        <div className="UseState">
+            <PageTitle
+                title="Hook UseState"
+                subtitle="Estado em componentes funcionais!"
+    
+            />
+            <SectionTitle title="Exercício #01"></SectionTitle>
+            <div className="center">
+               <span className="text">{x}</span>
+               <div>
+                    <button className="btn" onClick={
+
+                        () => setX(x + 1)
+
+                    }>+1</button>
+                    <button className="btn" onClick={
+
+                        () => setX(x - 1)
+
+                    }>-1</button> 
+               </div>
+            </div>
+            <SectionTitle title="Exercício #02"></SectionTitle>
+        </div>
+    )
+}
+
+export default UseState
