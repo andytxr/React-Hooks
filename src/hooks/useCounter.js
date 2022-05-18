56f8ec0c3d39613f4964f3fda7afed41aece0ca1
andytxr <andreluiiz.gst@gmail.com>
2022-05-18 17:48:50 -0300

    Creating a customized hook

---
+++ src/hooks/useCounter.js
@@ -0,0 +1,19 @@
+import React, {useState} from "react";
+
+export const useCounter = (initialValue = 100) => {
+
+    let [count, setCount] = useState(initialValue)
+
+    function inc(){
+
+        setCount(count + 1)
+
+    }
+    function dec(){
+
+        setCount(count -1)
+
+    }
+
+    return [count, inc, dec]
+}
